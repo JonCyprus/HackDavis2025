92c649a113c3322225f5a874decd7ec9f0a2bf6c
jay <jlight413@gmail.com>
2025-04-19 15:29:57 -0700

    react.js app added

---
+++ templates/src/App.js
@@ -0,0 +1,20 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>
+          Taskland
+        </h1>
+      </header>
+      <main>
+        <button className="cloudBtn">New Project</button>
+        
+      </main>
+    </div>
+  );
+}
+
+export default App;
