09b6b56e7615aa352e8154791ed85da39dd5f343
Kyan Wai <pi8765ggy2574@gmail.com>
2025-04-19 14:58:54 -0700

    JSON Structured Output
    Cerebras now outputs structured output, following the "taskSchema" JSON schema defined above.

--- source/cerebras_ai.py
+++ source/cerebras_ai.py
@@ -2,24 +2,41 @@ import os
 import json
 from cerebras.cloud.sdk import Cerebras # pip install --upgrade cerebras_cloud_sdk
 from dotenv import load_dotenv, find_dotenv # pip install python-dotenv
-from pathlib import Path
 
+# Find .env file, and pull the Cerebras API key. Attach a client.
 load_dotenv(find_dotenv())
-
 client = Cerebras(
     api_key=os.getenv("CEREBRAS_API_KEY")
 )
 
-stream = client.chat.completions.create(
-    messages=[
-        {
-            "role": "user",
-            "content": "Why is an apple red? Explain in five sentences or less.",
-        }
-    ],
+# Format the task data storage
+taskSchema = {
+    "type": "object",
+    "properties": {
+        "description": {"type": "string"},
+        "start_time": {"type": "string"},
+        "end_time": {"type": "string"},
+    },
+    "required": ["description", "start_time", "end_time"],
+    "additionalProperties": False
+}
+
+# Start a stream
+completion = client.chat.completions.create(
     model="llama-4-scout-17b-16e-instruct",
-    stream=True,
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant that generates movie recommendations."},
+        {"role": "user", "content": "Suggest a sci-fi movie from the 1990s"}
+    ],
+    response_format={
+        "type": "json_schema", 
+        "json_schema": {
+            "name": "task_schema",
+            "strict": True,
+            "schema": taskSchema
+        }
+    }
 )
 
-for chunk in stream:
-    print(chunk.choices[0].delta.content or "", end="")
+taskSchema = json.loads(completion.choices[0].message.content)
+print(json.dumps(taskSchema, indent=2))
