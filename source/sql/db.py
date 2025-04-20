0e532bdbd247c8dd507e2d01c53d5be8a190ca74
JonCyprus <joncyprus99@gmail.com>
2025-04-20 10:27:18 -0700

    remove circular import

--- source/sql/db.py
+++ source/sql/db.py
@@ -1,13 +1,11 @@
 import psycopg2
 from flask import g
-from server import app
 
 def get_db():
     if "db" not in g:
         g.db = psycopg2.connect(app.config['POSTGRES_URL'])
     return g.db
 
-@app.teardown_appcontext
 def close_db(exception=None):
     db = g.pop("db", None)
     if db:
