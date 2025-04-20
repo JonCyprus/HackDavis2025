bfacf4f541560a03ef04a263d69567b411089204
JonCyprus <joncyprus99@gmail.com>
2025-04-19 23:54:55 -0700

    fix quotes

--- source/sql/__init__.py
+++ source/sql/__init__.py
@@ -2,4 +2,4 @@ from . import db
 from .getAllUserTasks import getAllUserTasks
 from .createTask import createTask
 from .createUser import createUser
-__all__ = ["db", "getAllUserTasks", "createTask", createUser]
+__all__ = ["db", "getAllUserTasks", "createTask", "createUser"]
